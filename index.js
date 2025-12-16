912c01d08764e9e21edd43d31f11522f5b7d8643
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-12-16 16:25:18 +0200

    add addBook function

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import books from "./data.js";
 
 function getBookName(dataObj) {
-  const result = dataObj.map((book) => book.title)
-  .join(", ");
+  const result = dataObj.map((book) => book.title).join(", ");
 
   return result;
 }
 
-
 // console.log(getBookName(books));
 
 function getObjLength(dataObj) {
-    return dataObj.length;
+  return dataObj.length;
 }
 
-console.log(getObjLength(books));
+// console.log(getObjLength(books));
+
+function addBook(dataObj, bookTitle, bookPrice) {
+  dataObj.push({ title: bookTitle, price: bookPrice });
+
+  return dataObj;
+}
+
+console.log(addBook(books, "Knyga #6", 2.75));
+
+
