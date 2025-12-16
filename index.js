a8922d47adb1c495a88189d853f656212b9237bb
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-12-16 16:53:53 +0200

    add countDiscount function

--- index.js
+++ index.js
@@ -23,15 +23,31 @@ function addBook(dataObj, bookTitle, bookPrice) {
 // console.log(addBook(books, "Knyga #6", 2.75));
 
 function getLastBook(dataObj) {
-    return dataObj[dataObj.length - 1];
+  return dataObj[dataObj.length - 1];
 }
 
 // console.log(getLastBook(books));
 
 function countBookSum(dataObj) {
-    const result = dataObj.reduce((sum, {price}) => sum + price, 0);
+  const result = dataObj.reduce((sum, { price }) => sum + price, 0);
 
-    return result.toFixed(2);
+  return result.toFixed(2);
 }
 
-console.log(countBookSum(books));
+// console.log(countBookSum(books));
+
+function countDiscount(dataObj) {
+  const expensiveBooks = dataObj.filter(({ price }) => price > 10);
+
+  const booksWithDiscount = expensiveBooks
+    .map(({ price, title }) => ({bookTitle: title, oldPrice: price, newPrice: (price * 0.75).toFixed(2)}))
+
+  const result = booksWithDiscount.map(({bookTitle, oldPrice, newPrice}) => `Knyga: ${bookTitle} Sena kaina: ${oldPrice} Pardavimo kaina: ${newPrice} Pritaikyta nuolaida -25%`).join("\n");
+  
+  
+  return `!!! Nukainuota !!! \n${result}`;
+}
+
+// console.log(countDiscount(books));
+
+
